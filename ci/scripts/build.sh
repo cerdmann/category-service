9a00ff29c956fcd718226d0123b0fc76d605f6a3
Craig Erdmann <erds@badmoondev.com>
2017-01-21 19:18:21 -0600

    Merging first-pass-ci branch into master
    Single user pull and merge. Yep, I know, bad idea.

---
+++ ci/scripts/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "=============================================="
+echo "Beginning build of dotnet core web application"
+echo "dotnet core cli version $(dotnet --version)"
+echo "=============================================="
+
+cd git-repo
+
+dotnet restore
+
+dotnet publish -c release -r ubuntu.14.04-x64
+
+COMMIT=$(git rev-parse HEAD)
+ARTIFACT_NAME=category-service.$COMMIT.tar.gz
+
+echo $ARTIFACT_NAME > ../artifact/release_name.txt
+echo $(git log --format=%B -n 1 $COMMIT) > ../artifact/release_notes.md
+echo $COMMIT > ../artifact/release_commitish.txt
+
+tar -czvf ../artifact/$ARTIFACT_NAME ./bin/release/netcoreapp1.1/publish
+
+echo "----------------------------------------------"
+echo "Build Complete"
+ls -lah ../artifact
+echo "----------------------------------------------"
